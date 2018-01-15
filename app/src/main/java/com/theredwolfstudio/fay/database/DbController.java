1e35d408da04a717bc6d4cd2a604884b076f20b0
Koldur <koldur.c@gmail.com>
2018-01-15 11:31:26 +0100

    Task handler done

--- app/src/main/java/com/theredwolfstudio/fay/database/DbController.java
+++ app/src/main/java/com/theredwolfstudio/fay/database/DbController.java
@@ -1,8 +1,12 @@
 package com.theredwolfstudio.fay.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 /**
  * Created by Koldur on 04/01/2018.
@@ -16,12 +20,13 @@ public class DbController extends SQLiteOpenHelper {
     public static final String DB_EXAMP_COLUMN = "Name";
 
     public DbController(Context c){
-        super(c, this.DB_NAME, null, this.DB_VERS);
+
+        super(c, DB_NAME, null, DB_VERS);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT NOT NULL)", this.DB_TAB, this.DB_EXAMP_COLUMN);
+        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT NOT NULL);", this.DB_TAB, this.DB_EXAMP_COLUMN);
         db.execSQL(query);
     }
 
@@ -34,5 +39,30 @@ public class DbController extends SQLiteOpenHelper {
 
     public void insertSimpleTask(String name){
         SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DB_EXAMP_COLUMN, name);
+        db.insertWithOnConflict(DB_TAB, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+        db.close();
+    }
+
+    public void deleteSimpleTask(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DB_TAB, DB_EXAMP_COLUMN + " = ?", new String[]{name});
+        db.close();
+    }
+
+    public ArrayList<String> getTasksList(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<String> tasks = new ArrayList<String>();
+        Cursor c = db.query(DB_TAB, new String[]{DB_EXAMP_COLUMN}, null, null, null, null, null);
+
+        while(c.moveToNext()){
+            int index = c.getColumnIndex(DB_EXAMP_COLUMN);
+            tasks.add(c.getString(index));
+        }
+        c.close();
+        db.close();
+
+        return tasks;
     }
 }
